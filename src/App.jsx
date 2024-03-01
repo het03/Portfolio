927d69a5edd7837b1a4576d72b3b0da19cd475ec
Het Gajera <hr3362@srmist.edu.in>
2024-03-01 17:34:46 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='main'>
+    <div className='name-container'>
+    <div className='fname'>HET</div>
+    <div className='lname'>GAJERA</div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
