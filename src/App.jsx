ddebf594421af614582be21bfac7b12de8c2fed6
Het Gajera <hr3362@srmist.edu.in>
2024-03-06 20:40:54 +0530

    uhh still trying to fix hover on nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,57 @@
 import { useState } from 'react'
 import './App.css'
 
+function Header() {
+  return (
+  <header>
+    <div className='main'>
+      <div className='name-container'>
+        <div className='fname'>HET</div>
+        <div className='lname'>GAJERA</div>
+      </div>
+          <div className='title'>
+            <a href=''>Software Developer</a>
+          </div>
+    </div>
+  </header>
+  );
+}
+
+function Navigation() {
+  return (
+  <div className='nav'>
+    <div className='nav-container'>
+      <ul>
+        <li className='about grow'>ABOUT</li>
+        <li className='works grow'>WORKS</li>
+        <li className='resume grow'>RESUME</li>
+        <li className='contact grow'>CONTACT</li>
+      </ul>
+    </div>
+  </div>
+  );
+}
+
+function Footer() {
+  return (
+  <footer>
+    <div className='footer-container'>
+      <div className='linkedin'><a href=''>LinkedIn</a></div>
+      <div className='github'><a href=''>GitHub</a></div>
+      <div className='email'>
+        <a href=''>het36100@gmail.com</a>
+      </div>
+    </div>
+  </footer>
+  );
+}
+
 function App() {
   return (
     <>
-    <header>
-      <div className='main'>
-        <div className='name-container'>
-          <div className='fname'>HET</div>
-          <div className='lname'>GAJERA</div>
-        </div>
-            <div className='title'>
-              <a href=''>Software Developer</a>
-            </div>
-      </div>
-    </header>
-
-
-    <div className='nav'>
-      <div className='nav-container'>
-        <div className='nav-item 1'>ABOUT</div>
-        <div className='nav-item 2'>WORKS</div>
-        <div className='nav-item 3'>RESUME</div>
-        <div className='nav-item 4'>CONTACT</div>
-      </div>
-    </div>
-
-
-    <footer>
-      <div className='footer-container'>
-          <div className='linkedin'><a href=''>LinkedIn</a></div>
-          <div className='github'><a href=''>GitHub</a></div>
-          <div className='email'>
-            <a href=''>het36100@gmail.com</a>
-          </div>
-      </div>
-    </footer>
+      <Header />
+      <Navigation />
+      <Footer />
     </>
   )
 }
