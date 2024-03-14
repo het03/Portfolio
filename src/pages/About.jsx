7538356683fc4f77ef611fcbde0a745433c28572
Het Gajera <hr3362@srmist.edu.in>
2024-03-14 13:41:46 +0530

    about page transition

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,60 +1,64 @@
-import React from 'react';
-import './About.css'
+import React, {useEffect, useRef} from 'react';
+import { motion, useInView, useAnimation} from 'framer-motion';
+import './About.css';
 
 const About = () => {
+
+  const ref = useRef(null);
+  const isInView = useInView(ref, {once: true});
+
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if(isInView){
+      mainControls.start('visible');
+    }
+  }, [isInView]);
+
+
   return (
     <>
-    <div className='Top-container'>
-      <div className='Top'>
-        ABOUT
+      <div className='Top-container'>
+        <div className='Top'>
+          ABOUT
+        </div>
       </div>
-    </div>
-    <div className='Text-container'>
-      <div className='Text-box'>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Donec auctor, purus eget sollicitudin aliquam, nunc ex 
-          luctus odio, nec tincidunt nunc nunc nec ipsum. 
-          Suspendisse potenti
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Donec auctor, purus eget sollicitudin aliquam, nunc ex 
-          luctus odio, nec tincidunt nunc nunc nec ipsum. 
-          Suspendisse potenti
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Donec auctor, purus eget sollicitudin aliquam, nunc ex 
-          luctus odio, nec tincidunt nunc nunc nec ipsum. 
-          Suspendisse potenti
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Donec auctor, purus eget sollicitudin aliquam, nunc ex 
-          luctus odio, nec tincidunt nunc nunc nec ipsum. 
-          Suspendisse potenti
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Donec auctor, purus eget sollicitudin aliquam, nunc ex 
-          luctus odio, nec tincidunt nunc nunc nec ipsum. 
-          Suspendisse potenti
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Donec auctor, purus eget sollicitudin aliquam, nunc ex 
-          luctus odio, nec tincidunt nunc nunc nec ipsum. 
-          Suspendisse potenti
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Donec auctor, purus eget sollicitudin aliquam, nunc ex 
-          luctus odio, nec tincidunt nunc nunc nec ipsum. 
-          Suspendisse potenti
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Donec auctor, purus eget sollicitudin aliquam, nunc ex 
-          luctus odio, nec tincidunt nunc nunc nec ipsum. 
-          Suspendisse potenti
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Donec auctor, purus eget sollicitudin aliquam, nunc ex 
-          luctus odio, nec tincidunt nunc nunc nec ipsum. 
-          Suspendisse potenti
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Donec auctor, purus eget sollicitudin aliquam, nunc ex 
-          luctus odio, nec tincidunt nunc nunc nec ipsum. 
-          Suspendisse potenti
-        </p>
+      <div ref={ref} className='Text-container'>
+        <motion.div
+          className='Text-box'
+
+          variants={{
+            hidden: {opacity: 0, y: 75},
+            visible: {opacity: 1, y: 0},
+          }}
+          initial="hidden"
+          animate={mainControls}
+          transition={{ duration: 0.5, delay: 0.25 }}
+        >
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Donec auctor, purus eget sollicitudin aliquam, nunc ex 
+            luctus odio, nec tincidunt nunc nunc nec ipsum. 
+            Suspendisse potenti
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Donec auctor, purus eget sollicitudin aliquam, nunc ex 
+            luctus odio, nec tincidunt nunc nunc nec ipsum. 
+            Suspendisse potenti
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Donec auctor, purus eget sollicitudin aliquam, nunc ex 
+            luctus odio, nec tincidunt nunc nunc nec ipsum. 
+            Suspendisse potenti
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Donec auctor, purus eget sollicitudin aliquam, nunc ex 
+            luctus odio, nec tincidunt nunc nunc nec ipsum. 
+            Suspendisse potenti
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Donec auctor, purus eget sollicitudin aliquam, nunc ex 
+            luctus odio, nec tincidunt nunc nunc nec ipsum. 
+            Suspendisse potenti
+          </p>
+        </motion.div>
       </div>
-    </div>
     </>
   );
 };
