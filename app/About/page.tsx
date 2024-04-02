cfd7143d9f4ca24744d05011370fe5952afebbb3
Het Gajera <hr3362@srmist.edu.in>
2024-04-02 20:42:01 +0530

    Update about content

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,13 +1,13 @@
 'use client'
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import InitialPageTransition from '@/components/utils/InitialPageTransition';
 import './About.css';
+import Image, { StaticImageData } from 'next/image';
 import Reveal from '@/components/utils/Reveal';
+import RadarChart from '@/components/utils/RadarChart';
 
 export default function About() {
-
   const [showContent, setShowContent] = useState(false);
-
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowContent(true);
@@ -17,83 +17,55 @@ export default function About() {
 
   return (
     <>
-    <InitialPageTransition />
-    {showContent && (
-    <div className="about-container">
-      <div className="about-heading">
-        <div className="about-title">ABOUT</div>
-      </div>
-      <div className="text-container">
-        <div className="text-area">
-          <div className="text-box">
-            <div className="about">
-          <Reveal>
-            <div className="text">
-            <p>
-              Hi there! I&apos;m Het, a software engineer with a passion for both drawing and front-end web development.
-              Originally from Gujarat, India. I bring a wealth of enthusiasm and expertise to every project I undertake.
-            </p>
-            </div>
-          </Reveal>
-          <Reveal>
-            <div>
-              <p>
-              With a keen eye for detail and a knack for problem-solving, 
-              I specialize in leveraging front-end technologies such as React, HTML, CSS , JavaScript and more to create intuitive and visually stunning user interfaces.
-              Whether it&apos;s building responsive websites, optimizing user experiences, or implementing cutting-edge design trends, 
-              I thrive on pushing the boundaries of what&apos;s possible in web development.
-              </p>
-            </div>
-          </Reveal>
-          <Reveal>
-            <div>
-              <p>
-              If you&apos;re in search of a skilled and reliable front-end developer to bring your vision to life, I&apos;m here to help. Let&apos;s connect and turn your ideas into reality!
-              </p>
-            </div>
-          </Reveal>
-          <Reveal>
-          <a className="about-link" href="/Contact">Contact me</a>
-          </Reveal>
-          </div>
-          <div className="about-skills">
-            <div className="about-category">
+      <InitialPageTransition />
+      {showContent && (
+        <div className="about-container">
+
+          <section className='one'>
+            <div className="bg-img" />
+            <Reveal>
+            <h1 className='about-title'>ABOUT ME</h1>
+            </Reveal>
+          </section>
+
+          <section className='about-text'>
+            <div className="text-container">
               <Reveal>
-              <div className="skill-title">For Work</div>
+              <div className="text" id='a'>
+              Hi there! I'm Het, a software engineer with a passion for both drawing and front-end web development. Originally from Gujarat, India. I bring a wealth of enthusiasm and expertise to every project I undertake.
+              </div>
               </Reveal>
               <Reveal>
-                <div className="chip-container">
-                  <span className="chip">JavaScript</span>
-                  <span className="chip">CSS</span>
-                  <span className="chip">HTML</span>
-                  <span className="chip">React.js</span>
-                  <span className="chip">Figma</span>
-                  <span className="chip">Next.js</span>
+              <div className="text">
+              With a keen eye for detail and a knack for problem-solving, I specialize in leveraging front-end technologies such as React, HTML, CSS , JavaScript and more to create intuitive and visually stunning user interfaces. Whether it's building responsive websites, optimizing user experiences, or implementing cutting-edge design trends, I thrive on pushing the boundaries of what's possible in web development.
+              </div>
+              </Reveal>
+              <Reveal>
+              <div className="text">
+              If you're in search of a skilled and reliable front-end developer to bring your vision to life, I'm here to help. Let's connect and turn your ideas into reality!
+              </div>
+              </Reveal>
+              <Reveal>
+              <button className='contact-btn' onClick={() => window.location.href = '/Contact'}>CONTACT ME</button>
+              </Reveal>
+            </div>
+            <Reveal>
+            <Image src='/eva.jpg' alt='' width={600} height={900} objectFit='cover'/>
+            </Reveal>
+            </section>
+
+            <section>
+            <Reveal>
+              <div className="skill-page">
+                <div className="skill">
+                  SKILLS
                 </div>
-              </Reveal>
-            </div>
-            <div className="about-category">
-              <Reveal>
-                <div className="skill-title">For Fun</div>
-              </Reveal>
-              <Reveal>
-                <div className="chip-container">
-                  <span className="chip">TypeScript</span>
-                  <span className="chip">Angular</span>
-                  <span className="chip">Tailwind</span>
-                  <span className="chip">Python</span>
-                  <span className="chip">C</span>
-                  <span className="chip">C++</span>
-                  <span className="chip">React Native</span>
-                </div>
-              </Reveal>
-            </div>
-          </div>
-          </div>
+                <RadarChart />
+              </div>
+            </Reveal>
+            </section>
         </div>
-      </div>
-    </div>
-    )}
+      )}
     </>
   );
 }
