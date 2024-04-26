6da73dc8a0970fb68f24fe92965aab1732cac66f
Het Gajera <hr3362@srmist.edu.in>
2024-04-26 21:04:18 +0530

    fix

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, {useEffect, useState} from 'react';
 import InitialPageTransition from '@/components/utils/InitialPageTransition';
+import { PDFViewer } from '@react-pdf/renderer';
 import './resume.css'
 
 export default function Resume() {
@@ -19,11 +20,12 @@ export default function Resume() {
   <>
     <InitialPageTransition />
     {showContent && (
-      <>
-    <div className='resume-container' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
-      <h1>Will add shortly</h1>
+      <div className='resume-container'>
+        <div className="resume-heading">
+          <div className='resume-title'>RESUME</div>
+        </div>
+        <h1>will add shortly</h1>
     </div>
-    </>
     )}
     </>
   )
