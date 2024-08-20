402721be2bc73f2f49b284f2de34ec9fa12a5c38
Het Gajera <hr3362@srmist.edu.in>
2024-08-20 16:56:12 +0530

    About me overhaul

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -2,9 +2,8 @@
 import React, { useEffect, useState } from 'react';
 import InitialPageTransition from '@/components/utils/InitialPageTransition';
 import './About.css';
-import Image, { StaticImageData } from 'next/image';
+import Image from 'next/image';
 import Reveal from '@/components/utils/Reveal';
-import RadarChart from '@/components/utils/RadarChart';
 
 export default function About() {
   const [showContent, setShowContent] = useState(false);
@@ -19,59 +18,28 @@ export default function About() {
     <>
       <InitialPageTransition />
       {showContent && (
-        <div className="about-container">
-
-          <section className='one'>
-            <div className="bg-img" />
+        <div className="Acontainer">
+          <div className='head-container'>
+            <div className='Aheader'>
             <Reveal>
-            <h1 className='about-title'>ABOUT ME</h1>
-            </Reveal>
-          </section>
-
-          <section className='about-text'>
-            <div className='text-container'>
-              <Reveal>
-              <div className='text' id='a'>
-              Hi there! I&apos;m Het, a software engineer with a passion for both drawing and front-end web development. Originally from Gujarat, India. I bring a wealth of enthusiasm and expertise to every project I undertake.
-              </div>
+              <p>I&apos;m <span style={{fontWeight: 900}}><i>Het</i></span></p>
+            </Reveal>  
+            </div>
+          </div>
+          <div className='Amain'>
+          <Reveal>
+            <p>A <span style={{fontWeight: 900}}><i>software developer</i></span> with a passion front-end web development. I bring a wealth of enthusiasm and expertise to every project I undertake. With a keen eye for detail and a knack for problem-solving, I specialize in leveraging front-end technologies to create intuitive and visually stunning user interfaces.</p>
+          </Reveal>
+            <br />
+            <div className='connect'>
+            <Reveal>
+              <p>Feel free to connect!</p>
               </Reveal>
               <Reveal>
-              <div className='text'>
-              With a keen eye for detail and a knack for problem-solving, I specialize in leveraging front-end technologies such as React, HTML, CSS , JavaScript and more to create intuitive and visually stunning user interfaces. Whether it&apos;s building responsive websites, optimizing user experiences, or implementing cutting-edge design trends, I thrive on pushing the boundaries of what&apos;s possible in web development.
-              </div>
-              </Reveal>
-              <Reveal>
-              <div className='text'>
-              If you&apos;re in search of a skilled and reliable front-end developer to bring your vision to life, I&apos;m here to help. Let&apos;s connect and turn your ideas into reality!
-              </div>
-              </Reveal>
-              <Reveal>
-              <button className='contact-btn' onClick={() => window.location.href = '/Contact'}>CONTACT ME</button>
+              <button className='contact' onClick={() => window.location.href = '/Contact'}><span>CONTACT </span></button>
               </Reveal>
             </div>
-            <Reveal>
-            <div style={{ maxWidth: '500px' }} className='eva'>
-              <Image
-                src="/eva.jpg"
-                alt="my-artwork1"
-                width={300}
-                height={600}
-                layout="responsive"
-              />
-            </div>
-            </Reveal>
-            </section>
-
-            <section>
-            <Reveal>
-              <div className="skill-page">
-                <div className="skill">
-                  SKILLS
-                </div>
-                <RadarChart />
-              </div>
-            </Reveal>
-            </section>
+          </div>
         </div>
       )}
     </>
